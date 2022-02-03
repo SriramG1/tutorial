20b28f0fb64a7c09d75d0b502a0a107120db419a
SriramG1 <98871887+SriramG1@users.noreply.github.com>
2022-02-03 15:48:38 +0530

    Add files via upload

---
+++ program1to10.java
@@ -0,0 +1,62 @@
+package com.workouts;
+
+public class program1to10 {
+    public static void main(String[] args) {
+        //program 1
+        long mobileNo = 9876543210l;
+        System.out.println("Program 1:");
+        System.out.println("Mobile number is : "+mobileNo);
+
+        //program 2
+        byte a=10;
+        System.out.println("Program 2:");
+        System.out.println(a);
+
+        //Program 3
+        long b=6l;
+        int c=6;
+        System.out.println("Program 3:");
+        System.out.println(b==c);
+
+        //program 4
+        System.out.println("Program 4:");
+        System.out.println(6.02f==6.02d);
+
+        //program 5
+        short shortNumber = 150;
+        byte byteNumber=(byte) shortNumber;
+        System.out.println("Program 5:");
+        System.out.println(byteNumber);
+
+        //program 6
+        float floatNumber=150.9f;
+        int intNumber=(int)floatNumber;
+        System.out.println("Program 6:");
+        System.out.println(intNumber);
+
+        //program 7
+        char character7 ='7';
+        System.out.println("Program 7:");
+        int intNumber2 =(int)character7;
+        System.out.println(intNumber2);
+
+        //program 8
+        int number55 =55;
+        System.out.println("Program 8:");
+        char character55=(char)number55;
+        System.out.println(character55);
+
+        //program 9
+        System.out.println("Program 9:");
+        System.out.println(7+7);
+        System.out.println(7+7.0);
+        System.out.println(7+'7');
+
+        //program 10
+        System.out.println("Program 10:");
+        System.out.println(true);
+        System.out.println(false);
+        System.out.println(true&true);
+        System.out.println(true&false);
+    }
+}
